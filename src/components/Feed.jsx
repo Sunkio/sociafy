975a3fd1f07cda66d78103da6d07ec1aba5dfd7a
Tanja Schmidt <tagretha@gmail.com>
2023-10-22 09:37:43 +0200

    add posts to feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Box } from "@mui/material";
+import Post from "./Post.jsx";
 
 const Feed = () => {
   return (
-    <Box bgcolor="pink" flex={4} p={2}>
-      Feed
+    <Box flex={4} p={2}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </Box>
   );
 };
---
+++ src/components/Post.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { red } from "@mui/material/colors";
+import { Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material";
+
+const Post = () => {
+  return (
+    <Card sx={{ margin: 5 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            JD
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVert />
+          </IconButton>
+        }
+        title="Jane Doe"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="20%"
+        image="https://images.unsplash.com/photo-1630175860333-5131bda75071?auto=format&fit=crop&q=80&w=1170&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite sx={{ color: red[500] }} />}
+          />
+        </IconButton>
+        <IconButton aria-label="share">
+          <Share />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Post;
